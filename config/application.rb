8bfb8abaff45b7bb78e8c793200904e59f039e1c
leeshall <leeshall@yahoo.com>
2014-12-10 21:47:23 +0000

    add CSV upload code to companies

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+# added to support the csv uploader
+require 'csv'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 
 Enterpriseape::Application.routes.draw do
 
+  resources :companies do
+    collection { post :import }
+  end
+  
+
   devise_for :users
   get 'welcome/index'
 
